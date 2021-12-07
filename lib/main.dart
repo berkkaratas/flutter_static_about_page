455661ea13bedfc734e1356e1834c819930ffab2
Your Name <berk-karatas@protonmail.com>
2021-12-07 07:26:55 +0300

    Icon-Font-Card-ListTile using

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,8 +26,57 @@ class MyApp extends StatelessWidget {
                 ),
                 Text(
                   "Spotify",
-                  style: TextStyle(fontSize: 45),
+                  style: GoogleFonts.lato(
+                    fontSize: 45,
+                  ),
                 ),
+                Text(
+                  "Listen",
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                Container(
+                  width: 200,
+                  child: Divider(
+                    height: 30,
+                    color: Colors.white,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 45.0),
+                  color: Colors.green,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      size: 25,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      'admin@spotify.com',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                  height: 10,
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(horizontal: 45.0),
+                  color: Colors.green,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      size: 25,
+                      color: Colors.white,
+                    ),
+                    title: Text(
+                      '512 345 65 78',
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                  ),
+                )
               ],
             ),
           ),
